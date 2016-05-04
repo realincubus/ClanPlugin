5fd6829a0b8f3c356f1798aa634a6229297174f8
realincubus <kemnitz.stefan@googlemail.com>
2016-05-04 14:00:15 +0200

    added a file that will be used to find the optimizer plugin with cmake

---
+++ lib/cmake/optimizer_plugin-config.cmake
@@ -0,0 +1,23 @@
+# Try to find the optimizer_plugin library
+
+# OPTIMIZER_PLUGIN_FOUND       - System has optimizer_plugin lib
+# OPTIMIZER_PLUGIN_LIBRARY     - Library needed to use optimizer_plugin
+
+
+if (OPTIMIZER_PLUGIN_LIBRARY)
+	# Already in cache, be silent
+	set(OPTIMIZER_PLUGIN_FIND_QUIETLY TRUE)
+endif()
+
+find_library(OPTIMIZER_PLUGIN_LIBRARY NAMES ClanPlugin.so)
+
+if (OPTIMIZER_PLUGIN_LIBRARY)
+	message(STATUS "Library optimizer_plugin found =) ${OPTIMIZER_PLUGIN_LIBRARY}")
+else()
+	message(STATUS "Library optimizer_plugin not found =(")
+endif()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OPTIMIZER_PLUGIN DEFAULT_MSG OPTIMIZER_PLUGIN_LIBRARY)
+
+mark_as_advanced(OPTIMIZER_PLUGIN_LIBRARY)
